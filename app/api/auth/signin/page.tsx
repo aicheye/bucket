a73825eff1239a0ebddb09ec35561212e6759da1
Sean Yang <sean@seanyang.me>
2025-11-29 03:27:29 -0500

    feat: refactor SignInPage to include error handling with Suspense and separate ErrorAuth component

--- app/api/auth/signin/page.tsx
+++ app/api/auth/signin/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import AuthScreen from "../../../components/auth";
 
-export default function SignInPage() {
+function ErrorAuth() {
   const searchParams = useSearchParams();
+  const error = searchParams.get("error");
 
+  return <AuthScreen error={error} />;
+}
+
+export default function SignInPage() {
   return (
-    <AuthScreen error={searchParams.get("error")} />
+    <Suspense fallback={<AuthScreen />}>
+      <ErrorAuth />
+    </Suspense>
   );
 }
 
