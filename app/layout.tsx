9fe55d44a3de1fb3220d52c63bd1c78ee779432e
Sean Yang <sean@seanyang.me>
2025-12-03 17:37:53 -0500

    feat: add web app manifest and update favicon files for improved branding and PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,18 @@ import Providers from "./providers";
 export const metadata: Metadata = {
   title: APP_NAME,
   description: "The all-in-one course management tool",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: APP_NAME,
+  },
 };
 
 export default async function RootLayout({
